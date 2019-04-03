8004428dc9f828520cf3f5d1b1c8120ee7174a48
Grady <JG318682@davenportschools.org>
2019-04-03 10:39:40 -0500

    Initial commit

---
+++ GameZone#1/src/DoesItHaveLegs.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class DoesItHaveLegs {
+
+    public static void main(String[] args){
+
+        String animal;
+        String guess;
+
+        Scanner keyboard = new Scanner(System.in);
+
+
+        for {
+            System.out.println("Computer: Does the animal you are thinking of have legs?");
+
+
+
+
+        } while(guess != animal);
+
+    }
+}
